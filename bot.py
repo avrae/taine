3117b36d6b2de963041ad8a182caabb387c79d58
Dusk <dusk.argentum@gmail.com>
2018-07-26 22:19:29 -0400

    I hope this works

--- bot.py
+++ bot.py
@@ -12,12 +12,12 @@ from lib.reports import get_next_report_num, Report
 bot = commands.Bot(command_prefix="~")
 bot.db = JSONDB()
 
-TOKEN = os.environ.get("TOKEN")
-OWNER_ID = "187421759484592128"
-BUG_CHAN = "336792750773239809"  # AVRAE DEV "336792750773239809"
-DDB_CHAN = "463580965810208768"  # AVRAE DEV "463580965810208768"
-FEATURE_CHAN = "297190603819843586"  # AVRAE DEV "297190603819843586"
-TRACKER_CHAN = "360855116057673729"  # AVRAE DEV "360855116057673729"
+TOKEN = os.environ.get("TOKEN") # os.environ.get("TOKEN")
+OWNER_ID = "187421759484592128" # ZHU "187421759484592128"
+BUG_CHAN = "336792750773239809" # AVRAE DEV "336792750773239809" Make sure all 4 of these are unique, or else
+DDB_CHAN = "463580965810208768" # AVRAE DEV "463580965810208768" the bot might not work properly!
+FEATURE_CHAN = "297190603819843586" # AVRAE DEV "297190603819843586"
+TRACKER_CHAN = "360855116057673729" # AVRAE DEV "360855116057673729"
 REACTIONS = [
     "\U0001f640",  # scream_cat
     "\U0001f426",  # bird
@@ -60,10 +60,9 @@ async def on_message(message):
     elif message.channel.id == FEATURE_CHAN:  # feature-request
         match = re.match(r"\**Feature [Rr]equest\**\s?:?(.+?)\n", message.content)
         report_type = 'AFR'
-    elif message.channel.id == DDB_CHAN:  # bug-hunting-ddb
+    elif message.channel.id == DDB_CHAN: # bug-hunting-ddb
         match = re.match(r"\**What is the [Bb]ug\?\**:? ?(.+?)\n", message.content)
         report_type = 'DDB'
-
     if match:
         title = match.group(1)
         report_num = get_next_report_num(report_type)
@@ -149,7 +148,6 @@ async def canrepro(ctx, _id, *, msg=''):
     await bot.say(f"Ok, I've added a note to `{report.report_id}` - {report.title}.")
     await report.update(ctx)
 
-
 @bot.command(pass_context=True, aliases=['up'])
 async def upvote(ctx, _id, *, msg=''):
     """Adds an upvote to the selected feature request."""
@@ -158,7 +156,6 @@ async def upvote(ctx, _id, *, msg=''):
     report.commit()
     await bot.say(f"Ok, I've added a note to `{report.report_id}` - {report.title}.")
     await report.update(ctx)
-
 
 @bot.command(pass_context=True, aliases=['cnr'])
 async def cannotrepro(ctx, _id, *, msg=''):
@@ -169,7 +166,6 @@ async def cannotrepro(ctx, _id, *, msg=''):
     await bot.say(f"Ok, I've added a note to `{report.report_id}` - {report.title}.")
     await report.update(ctx)
 
-
 @bot.command(pass_context=True, aliases=['down'])
 async def downvote(ctx, _id, *, msg=''):
     """Adds a downvote to the selected feature request."""
@@ -178,7 +174,6 @@ async def downvote(ctx, _id, *, msg=''):
     report.commit()
     await bot.say(f"Ok, I've added a note to `{report.report_id}` - {report.title}.")
     await report.update(ctx)
-
 
 @bot.command(pass_context=True)
 async def note(ctx, _id, *, msg=''):
---
+++ reports.py
@@ -0,0 +1,203 @@
+import discord
+from cachetools import LRUCache
+
+from lib.jsondb import JSONDB
+
+db = JSONDB()  # something something instancing
+
+PRIORITY = {
+    -2: "Patch Pending", -1: "Resolved",
+    0: "P0: Critical", 1: "P1: Very High", 2: "P2: High", 3: "P3: Medium", 4: "P4: Low", 5: "P5: Trivial",
+    6: "Pending/Other"
+}
+VERI_EMOJI = {
+    -2: "\u2b07",  # DOWNVOTE
+    -1: "\u274c",  # CROSS MARK
+    0: "\u2139",  # INFORMATION SOURCE
+    1: "\u2705",  # WHITE HEAVY CHECK MARK
+    2: "\u2b06",  # UPVOTE
+}
+
+
+TRACKER_CHAN = "360855116057673729"  # AVRAE DEV "360855116057673729"
+
+
+class Report:
+    message_cache = LRUCache(maxsize=100)
+
+    def __init__(self, reporter: str, report_id: str, title: str, severity: int, verification: int, attachments: list,
+                 message: str, upvotes:int=0, downvotes:int=0):
+        self.reporter = reporter
+        self.report_id = report_id
+        self.title = title
+        self.severity = severity
+        self.verification = verification
+        self.upvotes = upvotes
+        self.downvotes = downvotes
+        self.attachments = attachments
+        self.message = message
+
+    @classmethod
+    def new(cls, reporter: str, report_id: str, title: str, attachments: list, message: str = None, severity: int = 6,
+            verification: int = 0, upvotes: int = 0, downvotes: int = 0):
+        return cls(reporter, report_id, title, severity, verification, attachments, message, upvotes, downvotes)
+
+    @classmethod
+    def from_dict(cls, report_dict):
+        return cls(**report_dict)
+
+    def to_dict(self):
+        return {
+            'reporter': self.reporter, 'report_id': self.report_id, 'title': self.title, 'severity': self.severity,
+            'verification': self.verification, 'upvotes': self.upvotes, 'downvotes': self.downvotes,
+            'attachments': self.attachments, 'message': self.message
+        }
+
+    @classmethod
+    def from_id(cls, report_id):
+        reports = db.jget("reports", {})
+        try:
+            return cls.from_dict(reports[report_id.upper()])
+        except KeyError:
+            raise Exception("Report not found.")
+
+    def commit(self):
+        reports = db.jget("reports", {})
+        reports[self.report_id] = self.to_dict()
+        db.jset("reports", reports)
+
+    def get_embed(self, detailed=False, ctx=None):
+        if self.report_id.startswith("AFR"):
+            embed = discord.Embed(color=0x00ff00)
+            embed.add_field(name="Added By", value=f"<@{self.reporter}>")
+            embed.add_field(name="Priority", value=PRIORITY.get(self.severity, "Unknown"))
+            embed.add_field(name="Votes", value="\u2b06" + str(self.upvotes) + "` | `\u2b07" + str(self.downvotes))
+            embed.set_footer(text=f"~report {self.report_id} for details | Vote with ~up/~down {self.report_id} [note]")
+        elif self.report_id.startswith("AVR"):
+            embed = discord.Embed(color=0xff0000)
+            embed.add_field(name="Added By", value=f"<@{self.reporter}>")
+            embed.add_field(name="Priority", value=PRIORITY.get(self.severity, "Unknown"))
+            embed.add_field(name="Verification", value=str(self.verification))
+            embed.set_footer(text=f"~report {self.report_id} for details | Verify with ~cr/~cnr {self.report_id} [note]")
+        elif self.report_id.startswith("DDB"):
+            embed = discord.Embed(color=0xe30910)
+            embed.add_field(name="Added By", value=f"<@{self.reporter}>")
+            embed.add_field(name="Priority", value=PRIORITY.get(self.severity, "Unknown"))
+            embed.add_field(name="Verification", value=str(self.verification))
+            embed.set_footer(text=f"~report {self.report_id} for details | Verify with ~cr/~cnr {self.report_id} [note]")
+        elif self.report_id.startswith("WEB"):
+            embed = discord.Embed(color=0x57235c)
+            embed.add_field(name="Added By", value=f"<@{self.reporter}>", inline=True)
+            embed.add_field(name="Priority", value=PRIORITY.get(self.severity, "Unknown"), inline=True)
+            embed.add_field(name="Verification", value=str(self.verification), inline=True)
+            embed.add_field(name="Votes", value="\u2b06" + str(self.upvotes) + "` | `\u2b07" + str(self.downvotes), inline=True)
+            embed.set_footer(text=f"~report {self.report_id} for details | Verify with ~cr/~cnr {self.report_id} [note], or vote with ~up/~down {self.report_id} [note]")
+        else:
+            embed = discord.Embed()
+            embed.set_footer(text=f"~report {self.report_id} for details | Verify with ~cr/~cnr {self.report_id} [note], or vote with ~up/~down {self.report_id} [note]")
+            embed.add_field(name="Added By", value=f"<@{self.reporter}>")
+            embed.add_field(name="Priority", value=PRIORITY.get(self.severity, "Unknown"))
+            embed.add_field(name="Verification", value=str(self.verification))
+        embed.title = f"`{self.report_id}` {self.title}"
+        embed.description = f"*{len(self.attachments)} notes*"
+        if detailed:
+            if not ctx:
+                raise ValueError("Context not supplied for detailed call.")
+            embed.description = f"*{len(self.attachments)} notes, showing first 10*"
+            for attachment in self.attachments[:10]:
+                user = ctx.message.server.get_member(attachment['author'])
+                msg = attachment['msg'][:1020] or "No details."
+                embed.add_field(name=f"{VERI_EMOJI.get(attachment['veri'], '')} {user}",
+                                value=msg)
+
+        return embed
+
+    def canrepro(self, author, msg):
+        if [a for a in self.attachments if a['author'] == author and a['veri']]:
+            raise Exception("You have already verified this report.")
+        attachment = {
+            'author': author,
+            'msg': msg,
+            'veri': 1
+        }
+        self.verification += 1
+        self.attachments.append(attachment)
+
+    def upvote(self, author, msg):
+        if [a for a in self.attachments if a['author'] == author and a['veri']]:
+            raise Exception("You have already upvoted this report.")
+        attachment = {
+            'author': author,
+            'msg': msg,
+            'veri': 2
+        }
+        self.upvotes += 1
+        self.attachments.append(attachment)
+
+    def cannotrepro(self, author, msg):
+        if [a for a in self.attachments if a['author'] == author and a['veri']]:
+            raise Exception("You have already verified this report.")
+        attachment = {
+            'author': author,
+            'msg': msg,
+            'veri': -1
+        }
+        self.verification -= 1
+        self.attachments.append(attachment)
+
+    def downvote(self, author, msg):  # lol Dusk was here
+        if [a for a in self.attachments if a['author'] == author and a['veri']]:
+            raise Exception("You have already downvoted this report.")
+        attachment = {
+            'author': author,
+            'msg': msg,
+            'veri': -2
+        }
+        self.downvotes += 1
+        self.attachments.append(attachment)
+
+    def addnote(self, author, msg):
+        attachment = {
+            'author': author,
+            'msg': msg,
+            'veri': 0
+        }
+        self.attachments.append(attachment)
+
+    async def get_message(self, ctx):
+        if self.message is None:
+            return None
+        elif self.message in self.message_cache:
+            return self.message_cache[self.message]
+        else:
+            msg = await ctx.bot.get_message(ctx.bot.get_channel(TRACKER_CHAN), self.message)
+            if msg:
+                Report.message_cache[self.message] = msg
+            return msg
+
+    async def update(self, ctx):
+        await ctx.bot.edit_message(await self.get_message(ctx), embed=self.get_embed())
+
+    async def resolve(self, ctx, msg=''):
+        if self.severity == -1:
+            raise Exception("This report is already closed.")
+
+        self.severity = -1
+        if msg:
+            self.addnote(ctx.message.author.id, f"Resolved - {msg}")
+
+        msg = await self.get_message(ctx)
+        if msg:
+            try:
+                await ctx.bot.delete_message(msg)
+                del Report.message_cache[self.message]
+            finally:
+                self.message = None
+
+
+def get_next_report_num(identifier):
+    id_nums = db.jget("reportnums", {})
+    num = id_nums.get(identifier, 0) + 1
+    id_nums[identifier] = num
+    db.jset("reportnums", id_nums)
+    return f"{num:0>3}"
