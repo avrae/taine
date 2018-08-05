55c5b14c1e7e1ccaf03b63e322ee384e3c579f51
Andrew Zhu <mommothazaz123@gmail.com>
2018-08-05 11:41:00 -0700

    make taine actually clear update cache

--- bot.py
+++ bot.py
@@ -297,6 +297,8 @@ async def update(ctx, build_id: int):
         await report.resolve(ctx, f"Patched in build {build_id}", ignore_closed=True)
         report.commit()
         changelog += f"- `{report.report_id}` {report.title}\n"
+
+    bot.db.jset("pending-reports", [])
     await bot.send_message(ctx.message.channel, changelog)
     await bot.delete_message(ctx.message)
 
