2d19f716a2993e2bcd03c4d0d44bab90b2f8f623
Andrew Zhu <me@andrew-zhu.com>
2019-03-04 17:01:55 -0800

    remove ~bug

--- bot.py
+++ bot.py
@@ -84,61 +84,6 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-@bot.command(pass_context=True, no_pm=True)
-async def bug(ctx):
-    """Reports a bug."""
-    try:
-        whatis_prompt = await bot.reply("What is the bug? (Say a short description)")
-        whatis = await bot.wait_for_message(timeout=300, author=ctx.message.author, channel=ctx.message.channel)
-        assert whatis is not None
-
-        def severity_check(msg):
-            return msg.content.lower() in ('trivial', 'low', 'medium', 'high', 'critical', 'other')
-
-        severity_prompt = await bot.reply(
-            "What is severity of the bug? (Trivial (typos, etc) / Low (formatting issues, "
-            "things that don't impact operation) / Medium (minor functional impact) / "
-            "High (a broken feature, major functional impact) / "
-            "Critical (bot crash, extremely major functional impact))")
-        severity = await bot.wait_for_message(timeout=120, author=ctx.message.author, channel=ctx.message.channel,
-                                              check=severity_check)
-        assert severity is not None
-
-        def len_check(maxlen):
-            return lambda m: len(m.content) < maxlen
-
-        repro_prompt = await bot.reply("How can I reproduce this bug? (Say a short description, 1024 char max)")
-        repro = await bot.wait_for_message(timeout=300, author=ctx.message.author, channel=ctx.message.channel,
-                                           check=len_check(1024))
-        assert repro is not None
-
-        context_prompt = await bot.reply("What is the context of the bug? "
-                                         "(The command and any choice trees that led to the bug, "
-                                         "256 char max)")
-        context = await bot.wait_for_message(timeout=300, author=ctx.message.author, channel=ctx.message.channel,
-                                             check=len_check(256))
-        assert context is not None
-    except AssertionError:
-        return await bot.reply("Timed out waiting for a response.")
-
-    report_id = f"AVR-{get_next_report_num('AVR')}"
-    title = whatis.content
-    details = f"**What is the bug?**: {title}\n" \
-              f"**Severity**: {severity.content.title()}\n" \
-              f"**Steps to reproduce**: {repro.content}\n" \
-              f"**Context**: {context.content}"
-
-    report = await Report.new(ctx.message.author.id, report_id, title,
-                              [{'author': ctx.message.author.id, 'msg': details, 'veri': 0}], author=ctx.message.author)
-    report_message = await bot.send_message(bot.get_channel(constants.TRACKER_CHAN), embed=report.get_embed())
-    report.message = report_message.id
-    report.commit()
-
-    await bot.say(f"Ok, submitting bug report. Keep track of `{report_id}`!")
-    await bot.delete_messages(
-        [whatis, severity, repro, context, whatis_prompt, severity_prompt, repro_prompt, context_prompt])
-
-
 @bot.command(pass_context=True, name="report")
 async def viewreport(ctx, _id):
     """Gets the detailed status of a report."""
