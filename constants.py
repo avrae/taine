488dbfba6aa223c1ad9acc709fce673f0ea8c662
Freddie <freddie@wafflemonster.org>
2019-06-18 16:29:49 -0700

    ♻️ Rename TOKEN to DISCORD_TOKEN for clarity
    - Move REACTIONS to constants.py

--- constants.py
+++ constants.py
@@ -5,3 +5,19 @@ FEATURE_CHAN = "297190603819843586"  # AVRAE DEV "297190603819843586"
 TRACKER_CHAN = "360855116057673729"  # AVRAE DEV "360855116057673729"
 WEB_CHAN = "487486995527106580"
 OWNER_GITHUB = "mommothazaz123"
+
+REACTIONS = [
+    "\U0001f640",  # scream_cat
+    "\U0001f426",  # bird
+    "\U0001f3f9",  # bow_and_arrow
+    "\U0001f989",  # owl
+    "\U0001f50d",  # mag
+    "bugs:454031039375867925",
+    "panic:354415867313782784",
+    "\U0001f576",  # sunglasses
+    "\U0001f575",  # spy
+    "\U0001f4e9",  # envelope_with_arrow
+    "\U0001f933",  # selfie
+    "\U0001f916",  # robot
+    "\U0001f409",  # dragon
+]
